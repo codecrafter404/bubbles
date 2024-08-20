d1126677c885063a700c0feac4349f96dcf5e27c
Codecrafter_404 <codecrafter404@github.com>
2024-08-20 21:17:02 +0200

    initial ui commit

---
+++ bubbles-ui/src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+</script>
+
+<main>
+  <h1
+    class="font-bold text-red-500 w-full text-center h-[100vh] content-center"
+  >
+    Hello world
+  </h1>
+</main>
+
+<style>
+</style>
