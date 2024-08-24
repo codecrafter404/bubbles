38e7ac7d1edc122bc36562c0f59ff140e6824a87
Codecrafter_404 <codecrafter404@github.com>
2024-08-24 11:37:47 +0200

    added cards

--- bubbles-ui/src/lib/Components/CreateSelectionItem.svelte
+++ bubbles-ui/src/lib/Components/CreateSelectionItem.svelte
@@ -3,17 +3,39 @@
 	import type { CustomItem, Item } from "../../generated/graphql";
 
 	export let item: Item | null;
+
 	export let customItem: CustomItem | null;
+	export let customItemPriceStart: number | null;
+	export let customItemPriceEnd: number | null;
+
+	export let hovered: boolean = true;
 </script>
 
-<div class="bg-primary-500 w-full h-full row-auto">
+<div
+	class={"ring-2 ring-opacity-30 aspect-[3/4] flex flex-col overflow-hidden rounded-md" +
+		(hovered ? " ring-accent-500" : "")}
+>
 	{#if item != null}
-		<img src={item.image} alt={item.name} class="object-contain" />
-		<p>{item.name}</p>
+		<img
+			src={item.image}
+			alt={item.name}
+			class="aspect-square w-full object-center object-cover flex-grow"
+		/>
 	{:else if customItem != null}
 		<p>{customItem.name}</p>
 	{:else}
 		<p>Invalid component Props</p>
 	{/if}
-	<p></p>
+	<div class="flex justify-between p-1 bg-primary-100">
+		<p class="max-w-[75%]">
+			{item ? item.name : customItem?.name}
+		</p>
+		<p class="font-bold">
+			{item
+				? item.price
+				: customItemPriceStart +
+					"€ -" +
+					customItemPriceEnd}€
+		</p>
+	</div>
 </div>
--- bubbles-ui/src/lib/Pages/CreateOrder.svelte
+++ bubbles-ui/src/lib/Pages/CreateOrder.svelte
@@ -9,32 +9,71 @@
 		client: getContextClient(),
 		query: gql.GetItemsForStoreDocument,
 	});
+	let current = 1;
+	let currentName = "Item 2";
+	function mapCustomItems(
+		customItems: Array<CustomItem>,
+		item: Array<Item>,
+	): Array<[CustomItem, number, number]> {
+		let res: Array<[CustomItem, number, number]> = customItems.map(
+			(x) => {
+				x.variants = x.variants.map(
+					(y) => item.find((z) => z.id == y.id)!,
+				);
+				return [x, 0, 0];
+			},
+		);
+		return res;
+	}
 </script>
 
-<div class="flex flex-col min-h-[100vh] 2xl:max-h-[100vh]">
-	<NavBar />
+<div class="flex flex-col min-h-[100vh] max-h-[100vh] h-[100vh]">
 	<div class="flex-grow flex flex-col 2xl:flex-row">
-		<div class="flex-grow bg-blue-100">
+		<div class="flex-grow max-h-full">
 			{#if $res.fetching}
 				<p>Loading</p>
 			{:else if $res.error}
 				<p>Failed to load: {$res.error.message}</p>
 			{:else if $res.data}
 				<div
-					class="grid overflow-x-scroll auto-rows-max grid-cols-1 gap-5"
+					class="overflow-y-scroll max-h-[100vh] h-[100vh] max-w-[70vw] p-3"
 				>
-					{#each $res.data.getItems as item}
-						{#if item.isOneOff && item.available}
+					<div
+						class="grid grid-cols-5 gap-3 items-center"
+					>
+						{#each $res.data.getItems as item}
+							{#if item.isOneOff && item.available}
+								<CreateSelectionItem
+									{item}
+									customItem={null}
+									customItemPriceEnd={null}
+									customItemPriceStart={null}
+									hovered={item.id ==
+										current &&
+										item.name ==
+											currentName}
+								/>
+							{/if}
+						{/each}
+						{#each mapCustomItems($res.data.getCustomItems, $res.data.getItems) as item}
 							<CreateSelectionItem
-								{item}
-								customItem={null}
+								item={null}
+								customItem={item[0]}
+								customItemPriceEnd={item[1]}
+								customItemPriceStart={item[2]}
+								hovered={item[0]
+									.id ==
+									current &&
+									item[0]
+										.name ==
+										currentName}
 							/>
-						{/if}
-					{/each}
+						{/each}
+					</div>
 				</div>
 			{/if}
 		</div>
-		<div class="2xl:w-[30vw] bg-orange-200">
+		<div class="w-[30vw] bg-orange-200">
 			OrderList (Shopping Cart)
 		</div>
 	</div>
